58096634e6bdec703e978836c81aa7a41f1f9e13
John D Pell <John+git@gaelicWizard.net>
2010-04-16 00:16:11 +0000

    Work around $PATH fucking stupid shit.

--- send2armitageMediaDrop.sh
+++ send2armitageMediaDrop.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -l
+#!/bin/bash --login
 
 set -e
 
 require trash || return 1
 
-nice -n 1 rsync -ahv --xattrs --inplace --progress --cvs-exclude "$@" armitage:Movies/Drop/
+nice -n 1 ~/Tools/rsync -ahv --xattrs --inplace --progress --cvs-exclude "$@" armitage:Movies/Drop/
     # Don't enable compression b/c ssh does compression itself.
     # --xattrs for extended attributes, including resource fork.
     # --inplace implies --resume, which speeds up my usage (since I'm almost universally just copying files one-way and I just want --resume anyway).
