1d931286621085c75db684523ee21dc57343cd43
John D Pell <John+git@gaelicWizard.net>
2010-04-16 00:13:30 +0000

    Update for rsync(1) version 3. (3.0.7, to be precise.)

--- send2armitageMediaDrop.sh
+++ send2armitageMediaDrop.sh
@@ -4,9 +4,9 @@ set -e
 
 require trash || return 1
 
-nice -n 1 rsync -ahv -E --inplace --progress --cvs-exclude "$@" armitage:Movies/Drop/
+nice -n 1 rsync -ahv --xattrs --inplace --progress --cvs-exclude "$@" armitage:Movies/Drop/
     # Don't enable compression b/c ssh does compression itself.
-    # -E / --extended-attributes (incl. resource fork)
+    # --xattrs for extended attributes, including resource fork.
     # --inplace implies --resume, which speeds up my usage (since I'm almost universally just copying files one-way and I just want --resume anyway).
     # --cvs-exclude imports ~/.cvsignore (and some defaults) for excludes
 
