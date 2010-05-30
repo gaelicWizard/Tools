2b2d8ee0f57844d4d076b3733ce82e0301a66083
John D Pell <John+git@gaelicWizard.net>
2010-05-30 20:24:53 +0000

    Correct typo (catmovies -> catmovie). Replace seq() with jot(1), since that's easier than making bash see custom-defined functions.

--- makeBleachRefs.sh
+++ makeBleachRefs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#function catmovie { echo "catmovie $@"; } # debug
+
 set -e
     # Be strict
 
@@ -7,7 +9,7 @@ INCLUDE_TRACKS=( -track "Video Track" -track Stereo -track English -track "Engli
     # Unless named, all languages will be named Stereo, and there's no easy way to filter.
 SKIP_CHAPTERS=( -nochapter Opening -nochapter "Opening   " -nochapter Closing -nochapter "Closing   " )
 
-for i in `seq 3 11` `seq 14 26`
+for i in `jot - 3 11` `jot - 14 26`
     # I failed to encode Volumes 12 & 13 with chapter markers, so they must be done separately.
     # Since I've made the executive decision to put the Bleach movies in with the TV Show, I need to separate discs 1 and 2 as well.
 do
@@ -16,20 +18,20 @@ do
         "$@"
 done
 
-catmovies -auto-chapters -force-same-tracks "${INCLUDE_TRACKS[@]}" "${SKIP_CHAPTERS[@]}" -o "Bleach: Volume 1".mov \
+catmovie -auto-chapters -force-same-tracks "${INCLUDE_TRACKS[@]}" "${SKIP_CHAPTERS[@]}" -o "Bleach: Volume 1".mov \
     /Volumes/DroboMedia/iTunes\ Media/TV\ Shows/Bleach/Season\ 1/1-*.m4v \
     "$@"
-catmovies -auto-chapters -force-same-tracks "${INCLUDE_TRACKS[@]}" "${SKIP_CHAPTERS[@]}" -o "Bleach: Volume 2".mov \
+catmovie -auto-chapters -force-same-tracks "${INCLUDE_TRACKS[@]}" "${SKIP_CHAPTERS[@]}" -o "Bleach: Volume 2".mov \
     /Volumes/DroboMedia/iTunes\ Media/TV\ Shows/Bleach/Season\ 1/2-*.m4v \
     "$@"
 
-catmovies -auto-chapters -force-same-tracks "${INCLUDE_TRACKS[@]}" "${SKIP_CHAPTERS[@]}" -o "Bleach: Volume 12".mov \
+catmovie -auto-chapters -force-same-tracks "${INCLUDE_TRACKS[@]}" "${SKIP_CHAPTERS[@]}" -o "Bleach: Volume 12".mov \
     /Volumes/DroboMedia/iTunes\ Media/TV\ Shows/Bleach/Season\ 3/12-01*.m4v -startAt 00:01:25.0 -trimTo 00:20:02.1 \
     /Volumes/DroboMedia/iTunes\ Media/TV\ Shows/Bleach/Season\ 3/12-02*.m4v -startAt 00:01:25.0 -trimTo 00:20:02.5 \
     /Volumes/DroboMedia/iTunes\ Media/TV\ Shows/Bleach/Season\ 3/12-03*.m4v -startAt 00:01:25.0 -trimTo 00:20:02.5 \
     /Volumes/DroboMedia/iTunes\ Media/TV\ Shows/Bleach/Season\ 3/12-04*.m4v -startAt 00:01:25.0 -trimTo 00:20:02.5 \
     "$@"
-catmovies -auto-chapters -force-same-tracks "${INCLUDE_TRACKS[@]}" "${SKIP_CHAPTERS[@]}" -o "Bleach: Volume 13".mov \
+catmovie -auto-chapters -force-same-tracks "${INCLUDE_TRACKS[@]}" "${SKIP_CHAPTERS[@]}" -o "Bleach: Volume 13".mov \
     /Volumes/DroboMedia/iTunes\ Media/TV\ Shows/Bleach/Season\ 3/13-01*.m4v -startAt 00:01:25.0 -trimTo 00:20:01.5 \
     /Volumes/DroboMedia/iTunes\ Media/TV\ Shows/Bleach/Season\ 3/13-02*.m4v -startAt 00:01:25.0 -trimTo 00:20:01.5 \
     /Volumes/DroboMedia/iTunes\ Media/TV\ Shows/Bleach/Season\ 3/13-03*.m4v -startAt 00:01:30.0 -trimTo 00:20:51.0 \
