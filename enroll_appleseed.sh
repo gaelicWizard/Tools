2182d1e64686fee31e13aa3cd326ed76bf294c15
John D Pell <John+git@gaelicWizard.net>
2014-03-23 00:00:00 +0000

    Create enroll_appleseed.sh

---
+++ enroll_appleseed.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/sbin/softwareupdate --set-catalog "https://swscan.apple.com/content/catalogs/others/index-10.9seed-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
