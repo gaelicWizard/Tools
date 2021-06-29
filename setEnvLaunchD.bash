dd1f78f96b640aaa4288394eeb8e20c42d29ab88
John D Pell <John+git@gaelicWizard.net>
2021-06-29 12:45:49 -0700

    Fix argument order to xpath…

--- setEnvLaunchD.bash
+++ setEnvLaunchD.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IFS=$'\n'
-for tehEnv in $(~/Tools/xpath /plist/dict/key ~/.MacOSX/environment.plist)
+for tehEnv in $(~/Tools/xpath ~/.MacOSX/environment.plist '/plist/dict/key')
 do
 	launchctl setenv "$tehEnv" "$( /usr/libexec/PlistBuddy -c 'Print :'"$tehEnv" ~/.MacOSX/environment.plist )"
 done
