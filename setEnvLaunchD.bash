26574bc3d0123d3a6a92ac84c2d453158bae0641
John D Pell <John+git@gaelicWizard.net>
2021-06-29 13:00:52 -0700

    Enable tilde-expansion for environment.plist

--- setEnvLaunchD.bash
+++ setEnvLaunchD.bash
@@ -3,5 +3,6 @@
 IFS=$'\n'
 for tehEnv in $(~/Tools/xpath ~/.MacOSX/environment.plist '/plist/dict/key')
 do
-	launchctl setenv "$tehEnv" "$( /usr/libexec/PlistBuddy -c 'Print :'"$tehEnv" ~/.MacOSX/environment.plist )"
+	tehVal="$( /usr/libexec/PlistBuddy -c 'Print :'"$tehEnv" ~/.MacOSX/environment.plist )"
+	launchctl setenv "$tehEnv" "${tehVal/#~/$HOME}"
 done
