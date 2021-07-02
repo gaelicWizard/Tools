20ac24d05d3760548594e2a4bf80fb8a84a0e339
John D Pell <John+git@gaelicWizard.net>
2021-07-01 20:04:36 -0700

    Attempt to expand environment variables in environment.plist

--- setEnvLaunchD.bash
+++ setEnvLaunchD.bash
@@ -3,6 +3,6 @@
 IFS=$'\n'
 for tehEnv in $(~/Tools/xpath ~/.MacOSX/environment.plist '/plist/dict/key')
 do
-	tehVal="$( /usr/libexec/PlistBuddy -c 'Print :'"$tehEnv" ~/.MacOSX/environment.plist )"
-	launchctl setenv "$tehEnv" "${tehVal/#~/$HOME}"
+    tehVal="$( /usr/libexec/PlistBuddy -c 'Print :'"$tehEnv" ~/.MacOSX/environment.plist | perl -pe 's/\$(\w+)/$ENV{$1}/g' )"
+    launchctl setenv "$tehEnv" "${tehVal/#~/$HOME}"
 done
